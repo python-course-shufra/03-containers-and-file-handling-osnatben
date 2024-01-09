90db56567fe781fac04ea9084946f2fc0462240e
osnatben <osnat969709@gmail.com>
2024-01-09 20:58:48 +0200

    res_leason

--- quiz_app.py
+++ quiz_app.py
@@ -1,24 +1,35 @@
-import sys
-import random
+proffesion=sys.argv[1]
+num_questions=int(sys.argv[2])
 
-
-def main():
-    # TODO: your code here
-
-    # 1. Get the command line arguments via sys.argv
-    sys.argv
-    arguments = sys.argv[1]
-    num_question=sys.argv[2]
+def view_question(text):
+    text=text.strip()
+    text=text.split(';')
+    question=text[0]
+    correct_answer=text[1]
+    options=(f'{text[2]},{text[1]}')
+    options=options.split(',')
+    random.shuffle(options)
+    return (question, options,correct_answer)
     
-    # 2. Open the correct file open(rf'questions\<filename>.txt)'
+    questions=[]
+    good_ans=0
+    with open(rf'questions\{proffesion}.txt', mode='r') as file:
+        for line in file:
+            questions.append(view_question(line))
 
+        random.shuffle(questions)
+        for i in range(num_questions):
+            print(questions[i][0])
+            for j, answer in enumerate(questions[i][1]):
+                print(f'{j+1}. {answer}')
 
-    # 3. Iterate over the file
+            print("enter the correct answer")
 
-    #       3.1. Parse the line (use s.split())
-    #       3.2 Create a list of options
-    #       3.3 random.shuffle(l)
-    pass
+            a=int(input())
+            if questions[i][1][a-1]==questions[i][2]:
+                good_ans+=1
+        
+        print(f'number of correct answers is {good_ans}/{num_questions}')
 
 
 if __name__ == '__main__':
